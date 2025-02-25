608f415fc09be7423b857caf5a4b626039213b55
Stanislava <glodzakova.s@gmail.com>
2025-02-25 04:40:27 +0100

    Changes for heroku

--- app.py
+++ app.py
@@ -79,4 +79,6 @@ def delete_cafe(cafe_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Get the PORT from environment variable, default to 5000 if not set
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
